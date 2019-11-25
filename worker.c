aa6acc4f144ee0af90fb7e07e448564933d2f877
Alexander Lausten <alexander@helius.dk>
2019-11-25 13:54:42 +0100

    Hooked up primes.c to worker

--- worker.c
+++ worker.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include "shared.h"
+#include "prime.h"
 #include <math.h>
 
 #define ARGV_WEIGHT 1
@@ -17,7 +18,7 @@
 #define REMOTE_PORT 5000
 
 
-struct result do_task(struct task new_task);
+struct result do_task(struct task new_task, weight_t weight);
 int connect_socket_n(int sock, struct sockaddr_in remote_address );
 int send_result(int sock, struct result send_message);
 struct task wait_for_next_task(int sock);
@@ -81,7 +82,7 @@ void socket_run_time(int sock, weight_t weight){
 		
 		printf("from %d, to %d, task num %d\n", buffer.from, buffer.to, buffer.task_number);
 
-		results = do_task(buffer);
+		results = do_task(buffer, weight);
 		send_result(sock, results);
 
 	}
@@ -90,52 +91,12 @@ void socket_run_time(int sock, weight_t weight){
 }
 
 
-int fmain(int argc, char *argv[]) {
-	int sock, connect_status;
-	ssize_t numRead;
-	weight_t weight = 2;
-	struct result resultn;
-	resultn.result = 5;
-	resultn.task_number = 1;
-	struct task buffer;
 
-	struct sockaddr_in local_adress, remote_address;
-
-	/* Initialise local socket */
-	local_adress.sin_family = AF_INET;
-	local_adress.sin_addr.s_addr = inet_addr("127.0.0.1");
-	local_adress.sin_port = LOCAL_PORT;
-	sock = socket(AF_INET, SOCK_STREAM, 0); /* Create client socket */
-
-	if (sock == -1){
-		exit(EXIT_FAILURE);
-	}
-
-	/* connect to the remote socket */
-	remote_address.sin_family = AF_INET;
-	remote_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	remote_address.sin_port = htons(5000);
-
-	connect_status = connect(sock, (struct sockaddr *)&remote_address, sizeof(remote_address));
-
-	/* send weight to master */
-	send(sock, &weight, sizeof(weight_t), 0);
-
-	send(sock, &resultn, sizeof(struct result), 0);
-	while ( (numRead = recv(sock, &buffer, sizeof(struct task), MSG_WAITALL)) > 0 ){
-		
-		printf("from %d, to %d, task num %d\n", buffer.from, buffer.to, buffer.task_number);
-
-
-	}
-}
-
-
-
-struct result do_task(struct task new_task) {
+struct result do_task(struct task new_task, weight_t weight) {
 	struct result new_result;
-	/* Implement  */
-	return new_result ;
+	new_result.result = weighted_prime_worker(weight, new_task);
+	new_result.task_number = new_task.task_number;
+	return new_result;
 }
 
 int connect_socket_n(int sock, struct sockaddr_in remote_address ) {
