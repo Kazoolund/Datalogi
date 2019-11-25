cd6d851ca6c8710ffae58b864a5d322a0bde34cf
Nikolaj <qvintusdk@gmail.com>
2019-11-25 14:17:41 +0100

    Changed type inputs for prime.c and prime.h

--- prime.h
+++ prime.h
@@ -2,11 +2,13 @@
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
+#include <inttypes.h>
+#include "shared.h"
 
 #define MAX_WEIGHT 100
 #define MILLISECOND_TO_SECOND 1000
 #define MICROSECOND_TO_MILLISECOND 1000
 
-int is_prime(int lower, int upper);
-int weighted_prime_worker(int weight, int lower, int upper);
+result_t is_prime(uint32_t from, uint32_t to);
+result_t weighted_prime_worker(weight_t weight, uint32_t from, uint32_t to);
 void sleep_ms(double msec);
