bfc461703488c8af5be2c7bd6f0b041255b05638
Alexander Lausten <alexander@helius.dk>
2019-11-25 13:35:17 +0100

    refactored worker

--- worker.c
+++ worker.c
@@ -8,25 +8,102 @@
 #include "shared.h"
 #include <math.h>
 
-#define PORT 8080
+#define ARGV_WEIGHT 1
 
-int is_prime(int lower, int upper);
+#define LOCAL_IP "127.0.0.1"
+#define LOCAL_PORT 8080
+#define REMOTE_IP "127.0.0.1"
+#define REMOTE_PORT 5000
 
-int main(void) {
+
+struct result do_task(struct task new_task);
+int connect_socket_n(int sock, struct sockaddr_in remote_address );
+int send_result(int sock, struct result send_message);
+struct task wait_for_next_task(int sock);
+
+void socket_run_time(int sock, weight_t weight);
+
+
+int main(int argc, char *argv[]) {
+	weight_t worker_weight = 0;
+	int sock;
+	struct sockaddr_in local_address;
+
+	if (worker_weight < 0){
+		printf("no input given for weight exiting....\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* initialse socket */
+	local_address.sin_family = AF_INET;
+	local_address.sin_addr.s_addr = inet_addr(LOCAL_IP);
+	local_address.sin_port = LOCAL_PORT;
+	sock = socket(AF_INET, SOCK_STREAM, 0); /* Create client socket */
+
+
+	/* check that socket was successfully initialized */
+	if (sock < 0){
+		printf("Error creating local socket exiting....\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	/* everything has progressed as expected so far, run the main program */
+	socket_run_time(sock, worker_weight);
+
+
+}
+
+void socket_run_time(int sock, weight_t weight){
+	struct sockaddr_in remote_address;
+	struct result results;
+	struct task buffer;
+	ssize_t numRead;
+	int connection_status;
+	int initial_send;
+	int send_result_n;
+
+	remote_address.sin_family = AF_INET;
+	remote_address.sin_addr.s_addr = inet_addr(REMOTE_IP);
+	remote_address.sin_port = htons(REMOTE_PORT);
+	/* Try to connect to master */
+	connection_status = connect_socket_n(sock, remote_address);
+	/* send weight to master */
+	initial_send = send(sock, &weight, sizeof(weight_t), 0);
+
+	if (initial_send < 0){
+		printf("Initial weight sent to master failed, exiting.... \n");
+		exit(EXIT_FAILURE);
+	}
+
+	
+	while ( (numRead = recv(sock, &buffer, sizeof(struct task), MSG_WAITALL)) > 0 ){
+		
+		printf("from %d, to %d, task num %d\n", buffer.from, buffer.to, buffer.task_number);
+
+		results = do_task(buffer);
+		send_result(sock, results);
+
+	}
+	
+
+}
+
+
+int fmain(int argc, char *argv[]) {
 	int sock, connect_status;
 	ssize_t numRead;
 	weight_t weight = 2;
 	struct result resultn;
 	resultn.result = 5;
 	resultn.task_number = 1;
-	char buffer[sizeof(struct task)];
-	struct task current_task;
+	struct task buffer;
+
 	struct sockaddr_in local_adress, remote_address;
 
 	/* Initialise local socket */
 	local_adress.sin_family = AF_INET;
 	local_adress.sin_addr.s_addr = inet_addr("127.0.0.1");
-	local_adress.sin_port = PORT;
+	local_adress.sin_port = LOCAL_PORT;
 	sock = socket(AF_INET, SOCK_STREAM, 0); /* Create client socket */
 
 	if (sock == -1){
@@ -40,20 +117,57 @@ int main(void) {
 
 	connect_status = connect(sock, (struct sockaddr *)&remote_address, sizeof(remote_address));
 
-	current_task.from = 5;
-	current_task.to = 10;
-	current_task.task_number = 1;
-
 	/* send weight to master */
-	send(sock, &current_task, sizeof(struct task), 0);
-	while ( (numRead = recv(sock, buffer, sizeof(struct task), MSG_WAITALL)) > 0 ){
-		printf("%s", buffer);
+	send(sock, &weight, sizeof(weight_t), 0);
+
+	send(sock, &resultn, sizeof(struct result), 0);
+	while ( (numRead = recv(sock, &buffer, sizeof(struct task), MSG_WAITALL)) > 0 ){
 		
+		printf("from %d, to %d, task num %d\n", buffer.from, buffer.to, buffer.task_number);
 
 
-		memset(buffer, 0, sizeof(struct task));
 	}
 }
+
+
+
+struct result do_task(struct task new_task) {
+	struct result new_result;
+	/* Implement  */
+	return new_result ;
+}
+
+int connect_socket_n(int sock, struct sockaddr_in remote_address ) {
+	int connect_status;
+	connect_status = connect(sock, (struct sockaddr *)&remote_address, sizeof(remote_address));
+	return connect_status;
+}
+
+int send_result(int sock, struct result send_message){
+	int send_status;
+	send_status = send(sock, &send_message, sizeof(struct result), 0);
+	if (send_status < 0){
+		printf("results failed, running again \n");
+		return send_result(sock, send_message);
+	}
+	return send_status;
+}
+
+struct task wait_for_next_task(int sock){
+	struct task buffer;
+	ssize_t numRead;
+	while ( (numRead = recv(sock, &buffer, sizeof(struct task), MSG_WAITALL)) > 0 ){
+		if (buffer.task_number != 0 && buffer.from > 0 && buffer.to > 0){
+			break;
+		}
+
+	}
+	return buffer;
+}
+
+
+
+
 /*
  * Function int is_prime(int lower, int upper) (FOR DEMO PURPOSES)
  * Takes two inputs: int lower and int upper.
