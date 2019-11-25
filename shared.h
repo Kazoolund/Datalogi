5bec7f10790cd91f86cdb6534cab30fe28f49977
Peter Mikkelsen <petermikkelsen10@gmail.com>
2019-11-25 10:49:21 +0100

    Her alexander :3

--- shared.h
+++ shared.h
@@ -1,9 +1,17 @@
 #include <stdint.h>
 
+#define LOADBALANCER_PORT 5000
+
 typedef uint8_t  weight_t;
 typedef uint16_t result_t;
 
 struct task {
 	uint32_t from;
 	uint32_t to;
+	uint16_t task_number;
+};
+
+struct result {
+	result_t result;
+	uint16_t task_number;
 };
