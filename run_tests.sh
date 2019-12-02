d613db91700c67b93f296a5757f9be3cc18ec1d1
Peter Mikkelsen <petermikkelsen10@gmail.com>
2019-12-02 13:22:10 +0100

    Run each test 5 times

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 for testname in tests/*; do
-    ./master $testname | tee test_results/`basename $testname`
+    ./master $testname | tee test_results/`basename $testname`run1
+    ./master $testname | tee test_results/`basename $testname`run2
+    ./master $testname | tee test_results/`basename $testname`run3
+    ./master $testname | tee test_results/`basename $testname`run4
+    ./master $testname | tee test_results/`basename $testname`run5
 done
